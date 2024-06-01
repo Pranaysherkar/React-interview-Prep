1499dc98d7e0635cc24510b64cb36709b97c2035
Pranay Sherkar <116248469+Pranaysherkar@users.noreply.github.com>
2024-06-01 20:15:00 +0530

    Swaping element using checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,57 @@
 import React, { useState } from "react";
-import Form from "./components/Form";
 
 function App() {
-  const arr = [
-    { title: "item1", checked: false },
-    { title: "item2", checked: false },
-    { title: "item3", checked: false },
-    { title: "item4", checked: false },
+  const arr1 = [
+    { title: "A", checked: false },
+    { title: "B", checked: false },
+    { title: "C", checked: false },
   ];
-  const arr2 = [
-    { title: "itemA", checked: false },
-    { title: "itemb", checked: false },
-    { title: "itemc", checked: false },
-    { title: "itemd", checked: false },
-  ];
-  const [list, setlist] = useState(arr);
+  const arr2 = [{ title: 1 }, { title: 3 }, { title: 2 }];
+  const [list1, setlist1] = useState(arr1);
   const [list2, setlist2] = useState(arr2);
-
-  const checkItems = (i) => {
-    const ulist1 = [...list];
-    ulist1[i].checked = !ulist1[i].checked;
-    setlist(ulist1);
+  const updatelist = (idx) => {
+    const Updatedlist1 = [...list1];
+    Updatedlist1[idx].checked = !Updatedlist1[idx].checked;
+    setlist1(Updatedlist1)
   };
-
-  const swapItems = () => {
-    const ulist1 = [...list];
-    const ulist2 = [...list2];
-    ulist1.map((e, idx) => {
-      if (e.checked) {
-        const temp = ulist1[idx].title;
-        ulist1[idx].title = ulist2[idx].title;
-        ulist2[idx].title = temp;
-        ulist1[idx].checked = false;
+  const swap=()=>{
+    const Updatedlist1 = [...list1];
+    const Updatedlist2 = [...list2];
+    Updatedlist1.map((e,i)=>{
+      if(e.checked){
+        const temp = Updatedlist1[i].title;
+        Updatedlist1[i].title = Updatedlist2[i].title
+        Updatedlist2[i].title = temp
+        Updatedlist1[i].checked=false;
+        setlist1(Updatedlist1)
+        setlist2(Updatedlist2)
       }
-      setlist(ulist1);
-      setlist2(ulist2);
-    });
-  };
+    })
 
+  }
+  console.log(list1);
   return (
-    <div className="h-screen w-full bg-black  p-20">
-      <div className="text-white">
-        <h1>List 1</h1>
+    <div className="h-screen w-screen bg-blue-400">
+      <div className="h-1/3 w-full flex flex-col item-center justify-center gap-14 text-xl p-20">
         <ul>
-          {list.map((l, i) => (
-            <li key={i}>
-              {" "}
+          {list1.map((elem, idx) => (
+            <li key={idx}>
               <input
                 type="checkbox"
-                checked={l.checked}
-                onClick={() => checkItems(i)}
-              />
-              {l.title}
+                checked={elem.checked}
+                onChange={() => updatelist(idx)}
+              />{" "}
+              {elem.title}
             </li>
           ))}
         </ul>
-      </div>
-      <hr />
-      <div className="mt-5 text-white">
-        <h1>List 2</h1>
         <ul>
-          {list2.map((l, i) => (
-            <li key={i}>{l.title}</li>
+          {list2.map((elem, idx) => (
+            <li key={idx}>{elem.title}</li>
           ))}
+        <input className="border px-2" onClick={swap} type="button" value="swap" />
         </ul>
       </div>
-      <button className="border mt-5 text-white" onClick={swapItems}>
-        swap{" "}
-      </button>
-        <Form />
     </div>
   );
 }
